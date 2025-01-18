6c2b57e6f8eb29875ec385dc128c1a3179465215
bhavik <bhavikvashi1804@gmail.com>
2025-01-18 16:29:09 +0530

    UI5 Controller Include another module

--- hello-world/webapp/controller/App.controller.js
+++ hello-world/webapp/controller/App.controller.js
@@ -1,8 +1,15 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], (Controller) => {
-  "use strict";
-  return Controller.extend("ui5.quickstart.App", {
-    sayHello: () => {
-      alert("Hello");
-    },
-  });
-});
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel",
+  ],
+  (Controller, MessageToast, JSONModel) => {
+    "use strict";
+    return Controller.extend("ui5.quickstart.App", {
+      sayHello: () => {
+        MessageToast.show("Hello World");
+      },
+    });
+  }
+);
